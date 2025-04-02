67385c04057ebf0f0b15726c2b2d60d46e080f6f
eltatata <davidtowo75@gmail.com>
2025-04-02 10:48:06 -0500

    feat/bycrypt adapter

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export * from './adapters/envs.adapter';
 export * from './adapters/zod.adapter';
+export * from './adapters/bcrypt.adapter';
 export * from './swagger/swagger.config';
 export * from './schemas/user-dtos.schemas';
