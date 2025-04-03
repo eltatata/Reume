a68d17be5a48144e616baecb4055006624ed7c0d
eltatata <davidtowo75@gmail.com>
2025-04-02 23:26:56 -0500

    feat/envs configuration updated

--- src/config/adapters/envs.adapter.ts
+++ src/config/adapters/envs.adapter.ts
@@ -3,4 +3,8 @@ import { get } from 'env-var';
 export const envs = {
   NODE_ENV: get('NODE_ENV').default('development').asString(),
   PORT: get('PORT').required().asPortNumber(),
+  DATABASE_URL: get('DATABASE_URL').required().asString(),
+  POSTGRES_USER: get('POSTGRES_USER').required().asString(),
+  POSTGRES_PASSWORD: get('POSTGRES_PASSWORD').required().asString(),
+  POSTGRES_DB: get('POSTGRES_DB').required().asString(),
 };
