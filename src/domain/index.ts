75098c355e5a19ff6d7e622098d618de3b133e32
eltatata <davidtowo75@gmail.com>
2025-04-04 15:00:55 -0500

    feat/remove duplicate export

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -4,7 +4,6 @@ export * from './interfaces/operations/operations.interfaces';
 export * from './interfaces/user/user.interfaces';
 export * from './interfaces/auth/register-user.interfaces';
 export * from './dtos/auth/register-user';
-export * from './dtos/auth/register-user';
 export * from './entities/user.entity';
 export * from './datasources/user/user.datasource';
 export * from './repositories/user/user.repository';
