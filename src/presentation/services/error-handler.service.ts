00abda50acc2d9ee092b434cfba1b2fb56c9ab7a
eltatataa <davidtowo75@gmail.com>
2025-04-29 23:15:39 -0500

    feature/enable the logs to be viewed in production

--- src/presentation/services/error-handler.service.ts
+++ src/presentation/services/error-handler.service.ts
@@ -1,14 +1,15 @@
 import { Response } from 'express';
-import { envs, logger } from '../../config';
+import { logger } from '../../config';
 import { CustomError } from '../../domain';
 
 export class ErrorHandlerService {
   static handleError(error: unknown, res: Response) {
     if (error instanceof CustomError) {
+      logger.warn(`${error}`);
       return res.status(error.statusCode).json({ error: error.message });
     }
 
-    if (envs.NODE_ENV === 'development') logger.error(`${error}`);
+    logger.error(`${error}`);
     return res.status(500).json({ error: 'Internal server error' });
   }
 }
