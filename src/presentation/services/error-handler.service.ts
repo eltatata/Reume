ad224fc0c6d00f042b5701de8772688d20834d9e
eltatataa <davidtowo75@gmail.com>
2025-04-29 21:37:16 -0500

    feature/logger in error handler

--- src/presentation/services/error-handler.service.ts
+++ src/presentation/services/error-handler.service.ts
@@ -1,5 +1,5 @@
 import { Response } from 'express';
-import { envs } from '../../config';
+import { envs, logger } from '../../config';
 import { CustomError } from '../../domain';
 
 export class ErrorHandlerService {
@@ -8,7 +8,7 @@ export class ErrorHandlerService {
       return res.status(error.statusCode).json({ error: error.message });
     }
 
-    if (envs.NODE_ENV === 'development') console.error(`Error: ${error}`);
+    if (envs.NODE_ENV === 'development') logger.error(`${error}`);
     return res.status(500).json({ error: 'Internal server error' });
   }
 }
