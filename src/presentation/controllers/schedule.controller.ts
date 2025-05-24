b3850e6838640bb2dd6696014fe87ed5c6f2f358
eltatata <davidtowo75@gmail.com>
2025-05-23 19:50:43 -0500

    feat/update use case controller

--- src/presentation/controllers/schedule.controller.ts
+++ src/presentation/controllers/schedule.controller.ts
@@ -1,7 +1,15 @@
 import { Response } from 'express';
 import { ErrorHandlerService, RequestExtended } from '../';
-import { CreateScheduleDTO, ScheduleRepository } from '../../domain';
-import { CreateSchedule, FindAllSchedules } from '../../application';
+import {
+  CreateScheduleDTO,
+  ScheduleRepository,
+  UpdateScheduleDTO,
+} from '../../domain';
+import {
+  CreateSchedule,
+  FindAllSchedules,
+  UpdateSchedule,
+} from '../../application';
 
 export class ScheduleController {
   constructor(private readonly scheduleRepository: ScheduleRepository) {}
@@ -22,6 +30,19 @@ export class ScheduleController {
   findAllSchedules = (req: RequestExtended, res: Response) => {
     new FindAllSchedules(this.scheduleRepository)
       .execute()
+      .then((data) => res.status(200).json(data))
+      .catch((error) => ErrorHandlerService.handleError(error, res));
+  };
+
+  updateSchedule = (req: RequestExtended, res: Response) => {
+    const { errors, validatedData } = UpdateScheduleDTO.create(req.body);
+    if (errors) {
+      res.status(400).json({ errors });
+      return;
+    }
+
+    new UpdateSchedule(this.scheduleRepository)
+      .execute(req.user!.id, req.params.id, validatedData!)
       .then((data) => res.status(200).json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
