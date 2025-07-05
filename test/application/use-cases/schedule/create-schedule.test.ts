7e37173510c1292c335327fc55ec5b88314a7c09
eltatata <davidtowo75@gmail.com>
2025-07-05 13:46:12 -0500

    feat/update of the mocks in the repository mocks

--- test/application/use-cases/schedule/create-schedule.test.ts
+++ test/application/use-cases/schedule/create-schedule.test.ts
@@ -5,6 +5,7 @@ describe('CreateSchedule', () => {
   const scheduleRepository = {
     findById: jest.fn(),
     findOverlapping: jest.fn(),
+    findByDate: jest.fn(),
     findAll: jest.fn(),
     create: jest.fn(),
     update: jest.fn(),
