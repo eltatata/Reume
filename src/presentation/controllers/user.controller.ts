22068ffd7b4307b34b947394d1a33fbe77cc1a57
eltatata <davidtowo75@gmail.com>
2025-07-17 00:42:00 -0500

    feat/controller to get users by id

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
-import { ErrorHandlerService } from '../';
+import { ErrorHandlerService, RequestExtended } from '../';
 import { UserRepository } from '../../domain';
-import { FindAllUsers } from '../../application';
+import { FindAllUsers, FindOneUser } from '../../application';
 
 export class UserController {
   constructor(private readonly userRepository: UserRepository) {}
@@ -9,6 +9,16 @@ export class UserController {
   findAllUsers = (req: Request, res: Response) => {
     new FindAllUsers(this.userRepository)
       .execute()
+      .then((data) => res.status(200).json(data))
+      .catch((error) => ErrorHandlerService.handleError(error, res));
+  };
+
+  findOneUser = (req: RequestExtended, res: Response) => {
+    const id = req.params.id;
+    const { id: userId, role } = req.user!;
+
+    new FindOneUser(this.userRepository)
+      .execute(id, userId, role)
       .then((data) => res.status(200).json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
