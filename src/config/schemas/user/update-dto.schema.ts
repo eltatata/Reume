35ee7889cf0fe8a055e79fecbed0fdd20224e1fa
eltatata <davidtowo75@gmail.com>
2025-07-25 20:57:33 -0500

    feat/validation for user roles in the user update department

--- src/config/schemas/user/update-dto.schema.ts
+++ src/config/schemas/user/update-dto.schema.ts
@@ -1,11 +1,16 @@
 import { z } from 'zod';
-import { UpdateUserDto } from '../../../domain';
+import { UpdateUserDto, UserRole } from '../../../domain';
 
 export const updateUserSchema: z.ZodType<UpdateUserDto> = z.object({
   firstname: z.string().trim().optional(),
   lastname: z.string().trim().optional(),
   email: z.string().trim().email('Invalid email format').optional(),
   password: z.string().trim().optional(),
+  role: z
+    .enum([UserRole.USER, UserRole.ADMIN], {
+      errorMap: () => ({ message: 'Invalid role. Must be USER or ADMIN.' }),
+    })
+    .optional(),
   verified: z.boolean().optional(),
   phone: z
     .string()
