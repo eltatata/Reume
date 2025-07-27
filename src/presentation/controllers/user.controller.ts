a9128575aff91bed7bf5eff2c0cb76e00eb93340
eltatata <davidtowo75@gmail.com>
2025-07-26 23:04:45 -0500

    feat/controller to update non-sensitive user information

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { ErrorHandlerService, RequestExtended } from '../';
-import { UserRepository } from '../../domain';
-import { FindAllUsers, FindOneUser } from '../../application';
+import { UserRepository, UpdateUserDto } from '../../domain';
+import { FindAllUsers, FindOneUser, UpdateUser } from '../../application';
 
 export class UserController {
   constructor(private readonly userRepository: UserRepository) {}
@@ -19,6 +19,22 @@ export class UserController {
 
     new FindOneUser(this.userRepository)
       .execute(id, userId, role)
+      .then((data) => res.status(200).json(data))
+      .catch((error) => ErrorHandlerService.handleError(error, res));
+  };
+
+  updateUser = (req: RequestExtended, res: Response) => {
+    const id = req.params.id;
+    const { id: userId, role } = req.user!;
+    const { errors, validatedData } = UpdateUserDto.create(req.body);
+
+    if (errors) {
+      res.status(400).json({ errors });
+      return;
+    }
+
+    new UpdateUser(this.userRepository)
+      .execute(id, userId, role, validatedData!)
       .then((data) => res.status(200).json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
